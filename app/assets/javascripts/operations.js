82355fb8c0ef7a17c56c29bf38a92f271e5920a2
balbesina <rabota.aaz@gmail.com>
2014-05-04 22:36:20 +0300

    js fix

--- app/assets/javascripts/operations.js
+++
@@ -1,9 +0,0 @@
-$(function () {    
-    $('#operation_is_income').change(function () {
-      var p = $('#operation_is_income').val();        
-      $.ajaxSetup({ cache: false });
-      $.get('<%= operation_select_categories_path(@operation) -%>', {is_income: p});        
-    });
-    $('#datetimepicker1').datetimepicker();
-    $('#datetimepicker2').datetimepicker();
-});
